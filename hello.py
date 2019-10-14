8c0b04a4a8f4d7fe5d85cad07de03d24b1bab3ae
huihui <1020284212@qq.com>
2019-10-14 11:46:54 +0800

    modify test

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ Git is a distributed version control system.
 Git is free software distributed under the GPL.
 Git has a mutable index called stage.
 Git tracks changes.
-Creating a new branch is quick and simple.
-love is love!
-believe it or not
+xixixidddggg
+
+
